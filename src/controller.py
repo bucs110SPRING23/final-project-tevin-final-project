ca23c506441a6d93470b06b4e9dd0082056f56a0
tevinflom <tflom1@binghamton.edu>
2023-05-02 23:47:42 -0400

    2

--- src/controller.py
+++ src/controller.py
@@ -2,6 +2,7 @@ import pygame
 from player import Player 
 
 
+
 pygame.init() 
 
 SCREEN_W = 1000 
@@ -14,17 +15,47 @@ pygame.display.set_caption("Object Detection Fighter Game")
 background = pygame.image.load("assets\wildwestbackground.jpg")
 scaled_background = pygame.transform.scale(background, screensize) 
 
-player_1 = Player(200, 300)
-player_2 = Player(700, 300)
+#spritesheets
 
-run_game = True 
-while run_game:
+huntress_spritesheet = pygame.image.load("assets/huntressfinalspritesheet.png").convert_alpha
+darkwizard_spritesheet = pygame.image.load("assets/wizardfinalspritesheet.png").convert_alpha
+
+#number of steps in each animation row
+
+huntress_phases = [5, 7, 8, 8, 2]
+darkwizard_phases = [8, 8, 6, 8, 2]
+
+#player variable 
+
+huntress_size = 162 
+huntress_data = [huntress_size]
+wizard_size = 200 
+wizard_data = [wizard_size]
+
+def health_bar(health, x, y): 
+    remaining_health = health / 100 
+    pygame.draw.rect(screen, "red", (x, y, 400, 25))
+    pygame.draw.rect(screen, "green", (x, y, 400 * remaining_health, 25))
+
+player_1 = Player(200, 300, huntress_data, huntress_spritesheet, huntress_phases)
+player_2 = Player(700, 300, wizard_data, darkwizard_spritesheet, darkwizard_phases)
+
+run = True
+# class Controller: 
+#     def __init__(self): 
+#         """
+#         add docs string later
+#         """
+    
+while run:
     #load background
     screen.blit(scaled_background, (0,0))
 
+    health_bar(player_1.health, 20, 20)
+    health_bar(player_2.health, 580, 20)
     #player motions
-    player_1.motion(SCREEN_W, SCREEN_H) 
-    #player_2.motion()
+    player_1.motion(SCREEN_W, SCREEN_H, screen, enemy = player_2) 
+    #player_2.motion
 
     #spawn players
     player_1.spawn(screen)
@@ -34,7 +65,8 @@ while run_game:
 
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: 
-            run_game = False 
+            run = False 
     pygame.display.update() 
+
 
 pygame.quit()
--- src/player.py
+++ src/player.py
@@ -6,14 +6,27 @@ pygame.init()
 
 
 class Player(): 
-    def __init__(self, x, y):
+    def __init__(self, x, y, data, spritesheet, animation):
         self.rect = pygame.Rect((x, y, 70, 170))
         self.jump_velocity = 0
+        self.flip = False
         self.jump = False
+        self.attacking = False
         self.attack_style = 0
+        self.health = 100
+        self.size = data[0]
 
-    def motion (self, screen_w, screen_h): 
-        SPEED = 5
+    def load_sprites(self, spritesheet, phases): 
+        y = 0
+        for animation in phases: 
+            img_list = []
+            for x in range(animation): 
+                individual_img = spritesheet.subsurface(x * self.size, 0, self.size, self.size)
+                img_list.append(individual_img)
+            y += 1
+
+    def motion (self, screen_w, screen_h, surface, enemy): 
+        SPEED = 2
         GRAV = 2
         x_pos = 0 
         y_pos = 0
@@ -24,22 +37,24 @@ class Player():
         #player_2 ctrls = leftarrow, rightarrow, uparrow for jump
 
         ## KEY CONTROLLED MOTION ##
-        if keypress[pygame.K_b]: 
-            x_pos = -SPEED
-        if keypress[pygame.K_f]: 
-            x_pos = SPEED
-        if keypress[pygame.K_j] and self.jump == False: 
-            self.jump_velocity = -20
-            self.jump = True #come back to this
+        if self.attacking == False:
+            if keypress[pygame.K_b]: 
+                x_pos = -SPEED
+            if keypress[pygame.K_f]: 
+                x_pos = SPEED
+            if keypress[pygame.K_j] and self.jump == False: 
+                self.jump_velocity = -20
+                self.jump = True #come back to this
 
-        ## ATTACKS - Regular and Special ## p1a = reg, p1s = spec; p2shift = reg, p2enter = spec
-        if keypress[pygame.K_a] or keypress[pygame.K_s]: 
-            self.attack_type = attacker() # ** 
-            # attack types # 
-            if keypress[pygame.K_a]: 
-                self.attack_style = 1 
-            if keypress[pygame.K_s]: 
-                self.attack_style = 2
+            ## ATTACKS - Regular and Special ## p1a = reg, p1s = spec; p2shift = reg, p2enter = spec
+            if keypress[pygame.K_a] or keypress[pygame.K_s]: 
+                self.attack(surface, enemy) # ** 
+                # attack types # 
+                if keypress[pygame.K_a]: 
+                    self.attack_style = 1 
+                if keypress[pygame.K_s]: 
+                    self.attack_style = 2
+        
 
         ## COME BACK DOWN AFTER JUMP ##
         self.jump_velocity += GRAV
@@ -56,13 +71,22 @@ class Player():
             self.jump = False
             y_pos = (screen_h - 120 - self.rect.bottom)
        
+        # keep players facing each other 
 
+        if enemy.rect.centerx > self.rect.centerx:
+            self.flip = False 
+        else: 
+            self.flip = True 
         ## UPDATE PLAYER POSITION ##
         self.rect.x += x_pos
         self.rect.y += y_pos
 
-    def attack(self, surface): 
-        attacker = pygame.Rect(self.rect.centerx, self.rect.y, 2 * self.rect.width, self.rect.height)
+    def attack(self, surface, enemy): 
+        self.attacking = True
+        attacker = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
+        #collision detector
+        if attacker.colliderect(enemy.rect):
+            enemy.health -= 10
         pygame.draw.rect(surface, (0, 210, 111), attacker)
 
     def spawn(self, surface): 
